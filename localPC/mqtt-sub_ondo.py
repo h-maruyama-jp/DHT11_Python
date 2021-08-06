0725bda295fbe80eff8acd88ade37e7843605068
h-maruyama-jp <h.maruyama0@gmail.com>
2021-08-07 08:56:23 +0900

    pass delete

--- localPC/mqtt-sub_ondo.py
+++ localPC/mqtt-sub_ondo.py
@@ -29,7 +29,7 @@ RedisKey = "RPIvalue"
 
 RedisHost = "redis-16303.c100.us-east-1-4.ec2.cloud.redislabs.com"  
 RedisPort = "16303"
-RedisPwd = "CTFaaddfoG014Km6QC6QQ1ueaKh9hIE3"
+RedisPwd = ""
 ##########################
 
 ###########################################################
